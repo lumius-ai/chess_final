efd22992ee9f8989960d948b5aa823eba8d8ccb8
Razvan Rotundu <raz.rotundu@gmail.com>
2024-11-08 15:44:44 -0500

    finish testing coords module

--- lib/chess_coords.rb
+++ lib/chess_coords.rb
@@ -1,5 +1,5 @@
 # Module containing the functions to convert between chess board coordinates and array coordinates
-
+require "pry-byebug"
 module ChessCoords
   COLUMNS = {'A' => 0,
   'B' => 1,
@@ -13,7 +13,12 @@ module ChessCoords
 
   # Convert a board notation position into a 2 digit row-column array
   def board_to_array(str)
+
     pre = str.split("")
+    if not COLUMNS.has_key?(pre[0]) or pre[1].to_i < 0 or pre[1].to_i > 8 or pre.length != 2
+      return nil
+    end
+
     out = Array.new(2)
 
     out[0] = 8 - pre[1].to_i
@@ -25,10 +30,13 @@ module ChessCoords
   # Convert a 2 digit array into board notation
   # [0, 0]
   def array_to_board(arr)
-    out = ""
+    if arr.length != 2 or arr[0] < 0 or arr[0] > 7 or arr[1] < 0 or arr[1] > 7
+      return nil
+    end
 
+    out = ""
     out += ChessCoords::COLUMNS.key(arr[1])
-    out += (8 - arr[0].to_i)
+    out += (8 - arr[0].to_i).to_s()
 
     return out
   end
